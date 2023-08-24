127f8289b9aacf9e8a9becd8bb4f59fee69d456b
tymkiwdylan <datymk23@colby.edu>
2023-08-24 10:33:31 -0300

    redux working

---
+++ smart-mozo/src/store/restaurantSlice.ts
@@ -0,0 +1,33 @@
+// restaurantSlice.ts
+
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+interface RestaurantState {
+  restaurant: Restaurant | null;
+}
+
+const initialState: RestaurantState = {
+  restaurant: null,
+};
+
+// Try to load the restaurant data from local storage
+const storedRestaurant = localStorage.getItem('restaurant');
+if (storedRestaurant) {
+  initialState.restaurant = JSON.parse(storedRestaurant);
+}
+
+const restaurantSlice = createSlice({
+  name: 'restaurant',
+  initialState,
+  reducers: {
+    setRestaurant: (state, action: PayloadAction<Restaurant>) => {
+      state.restaurant = action.payload;
+      // Save the updated restaurant data to local storage
+      localStorage.setItem('restaurant', JSON.stringify(action.payload));
+    },
+  },
+});
+
+export const { setRestaurant } = restaurantSlice.actions;
+
+export default restaurantSlice.reducer;
